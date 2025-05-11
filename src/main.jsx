18e34dfa9f8a467bacd01d24c02bd9f561fbf6d7
renan <renanbazinin2@gmail.com>
2025-05-11 12:36:37 +0300

    redirect option

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Component to handle external redirect
+const ExternalRedirect = ({ to }) => {
+  React.useEffect(() => {
+    window.location.href = to;
+  }, [to]);
+  
+  return <p>Redirecting to wheel...</p>;
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HashRouter>
+      <Routes>
+        {/* Main app route */}
+        <Route path="/" element={<App />} />
+        
+        {/* Redirect to external site */}
+        <Route 
+          path="/toWheel" 
+          element={<ExternalRedirect to="https://renabazinin.github.io/wheel/" />} 
+        />
+        
+        {/* Catch-all route for any other paths */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>,
 )
